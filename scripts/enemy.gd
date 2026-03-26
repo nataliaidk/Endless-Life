b99f3176c601aefdb0eaec6b45a9de3cccfaa9ad
marg4ryn <poweski.jan@gmail.com>
2026-03-26 16:21:11 +0100

    Collisions

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 class_name Enemy
 
 @export var max_health := 10
-@export var speed := 100
+@export var speed := 75
 @export var damage := 1
 @export var enemy_sprite: Sprite2D
 @export var player = null
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var health := max_health
 
@@ -30,3 +32,16 @@ func _physics_process(_delta):
 		var direction = global_position.direction_to(player.global_position)
 		velocity = direction * speed
 		move_and_slide()
+		update_animation(direction)
+
+func update_animation(direction: Vector2):
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			sprite.play("walk_right")
+		else:
+			sprite.play("walk_left")
+	else:
+		if direction.y > 0:
+			sprite.play("walk_down")
+		else:
+			sprite.play("walk_up")
