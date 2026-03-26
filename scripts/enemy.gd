f6e9f6790350ad9e44423e8cdbd233f88dba2a9b
marg4ryn <poweski.jan@gmail.com>
2026-03-26 22:50:54 +0100

    sprites

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,38 +1,23 @@
 extends CharacterBody2D
 
-class_name Enemy
-
-@export var max_health := 10
-@export var speed := 75
-@export var damage := 1
-@export var enemy_sprite: Sprite2D
-@export var player = null
-
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var progress_bar: ProgressBar = $ProgressBar
 
+var speed := 75
+var max_health := 10
+var damage := 5
 var health := max_health
-
-func _ready():
-	player = get_tree().get_first_node_in_group("player")
-	health = max_health
-	if enemy_sprite:
-		add_child(enemy_sprite)
-
-func take_damage(amount: int):
-	health -= amount
-	$ProgressBar.value = health
-	if health <= 0:
-		die()
-
-func die():
-	queue_free()
+var is_dead := false
 
 func _physics_process(_delta):
-	if player:
-		var direction = global_position.direction_to(player.global_position)
-		velocity = direction * speed
-		move_and_slide()
-		update_animation(direction)
+	if is_dead or player == null:
+		return
+		
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * speed
+	move_and_slide()
+	update_animation(direction)
 
 func update_animation(direction: Vector2):
 	if abs(direction.x) > abs(direction.y):
@@ -45,3 +30,24 @@ func update_animation(direction: Vector2):
 			sprite.play("walk_down")
 		else:
 			sprite.play("walk_up")
+			
+func _on_hitbox_body_entered(body):
+	if is_dead:
+		return
+	if body.is_in_group("player"):
+		print("damage dealt")
+		body.take_damage(damage)
+
+func take_damage(amount: int):
+	if is_dead:
+		return
+	health -= amount
+	progress_bar.value = health
+	if health <= 0:
+		die()
+
+func die():
+	is_dead = true
+	velocity = Vector2.ZERO
+	sprite.play("die")
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,22 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var camera := $Camera2D
+@onready var progress_bar: ProgressBar = $ProgressBar
+
+var speed := 200.0
+var max_health := 100
+var health := 100
+var is_dead := false
 
 func _ready():
 	camera.make_current()
 
 func _physics_process(_delta):
+	if is_dead:
+		return
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * SPEED
+	velocity = direction * speed
 	move_and_slide()
 	update_animation(direction)
 
@@ -28,3 +34,16 @@ func update_animation(direction: Vector2):
 				sprite.play("walk_down")
 			else:
 				sprite.play("walk_up")
+
+func take_damage(amount: int):
+	if is_dead:
+		return
+	progress_bar.value = health
+	health -= amount
+	if health <= 0:
+		die()
+		
+func die():
+	is_dead = true
+	velocity = Vector2.ZERO
+	sprite.play("die")
